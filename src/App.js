42df797a732a924a66255efc4a34ea1508ae768a
thatguychrisw <thatguychrisw@gmail.com>
2020-06-06 23:58:24 -0400

    ✊ initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,92 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component'
+import 'react-vertical-timeline-component/style.min.css'
+import './assets/main.css'
+import { GiCandleLight } from 'react-icons/gi'
+import { FaFistRaised } from 'react-icons/fa'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import victimData from './data/victims.js'
+
+function App () {
+    const introduction = (
+      <VerticalTimelineElement
+        className="border-b-0"
+        iconClassName="bg-gray-900 text-yellow-500"
+        textClassName="bg-gray-800 text-white rounded-none shadow-none"
+        contentArrowStyle={{borderRight: '0'}}
+        icon={<FaFistRaised/>}
+      >
+          <h3 className="text-2xl font-bold text-yellow-400">Black Victims of Police Murders</h3>
+          <p>
+              The following are victims of police brutality starting with <strong>Eric Gardner</strong> in 2014.
+              This list is from NPR.
+
+          </p>
+          <h5 className="text-sm pt-4">Last Updated June 6th, 2020</h5>
+      </VerticalTimelineElement>
+    )
+
+    const whatYouCanDo = (action) => (
+      <VerticalTimelineElement
+        className="border-b-0"
+        iconClassName="bg-gray-900 text-yellow-500"
+        textClassName="bg-gray-800 text-white rounded-none shadow-none"
+        contentArrowStyle={{borderRight: '0'}}
+        icon={<FaFistRaised/>}
+      >
+          <FaFistRaised className="text-4xl float-left pr-4"/>
+          <h3 className="text-2xl font-bold text-yellow-400">What can I do to help?</h3>
+          {action}
+      </VerticalTimelineElement>
+    )
+
+    const victims = victimData.map((victim, key) => {
+        const borderColor = key % 2 === 0 ? 'border-blue-500' : 'border-blue-500'
+        const backgroundColor = key % 2 === 0 ? 'bg-blue-500' : 'bg-blue-500'
+
+        return (
+          <React.Fragment key={`fragment-${key}`}>
+              {key > 0 && key % 12 === 0 &&
+              whatYouCanDo((
+                <ul>
+                    <li>An action item</li>
+                    <li>An action item</li>
+                    <li>An action item</li>
+                    <li>An action item</li>
+                    <li>An action item</li>
+                </ul>
+              ))
+              }
+
+              <VerticalTimelineElement
+                className="border-b-0"
+                key={key}
+                textClassName={`bg-white text-black border-t-4 rounded-none ${borderColor}`}
+                contentArrowStyle={{borderRight: '7px solid rgb(255, 255, 255)'}}
+                date={victim.date}
+                dateClassName="text-white"
+                iconClassName={`${backgroundColor} text-black`}
+                icon={<GiCandleLight/>}
+              >
+                  <h3 className="text-4xl">{victim.title}</h3>
+                  <h4 className="text-xl font-bold">{victim.fullName}</h4>
+                  <p>{victim.body}</p>
+              </VerticalTimelineElement>
+          </React.Fragment>
+        )
+    })
+
+    return (
+      <div className="bg-gray-800">
+          <VerticalTimeline className="text-white">
+              {introduction}
+
+              {victims}
+
+              {whatYouCanDo('You can donate.')}
+          </VerticalTimeline>
+      </div>
+    )
 }
 
-export default App;
+export default App
---
+++ src/data/victims.js
@@ -0,0 +1,702 @@
+export default [
+  {
+    "fullName": "Eric Garner",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "John Crawford III",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Michael Brown",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Ezell Ford",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Dante Parker",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Michelle Cuesseaux",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Laguan Mcdonald",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "George Mann",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Tanisha Anderson",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Akai Gurley",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Tamir Rice",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Rumain Brisbon",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Jerame Reid",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Matthew Ajibade",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Frank Smart",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Natasha Mckenna",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Tony Robinson",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Anthony Hill",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Mya Hall",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Phillip White",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Eric Harris",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Walter Scott",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "William Chapman II",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Alexia Christian",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Brendon Glenn",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Victor Manuel Larosa",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Jonathan Sanders",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Freddie Blue",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Joseph Mann",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Salvado Ellswood",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Sandra Bland",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Albert Joseph Davis",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Darrius Stewart",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Billy Ray Davis",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Samuel Dubose",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Michael Sabbie",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Brian Keith Day",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Christian Taylor",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Troy Robinson",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Asshams Pharaoh Manley",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Felix Kumi",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Keith Harrison Mcleod",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Junior Prosper",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Lamontez Jones",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Paterosn Brown",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Dominic Hutchinson",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Anthony Ashford",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Alonzo Smith",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Tyree Crawford",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "India Kager",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "La’vante Biggs",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Michael Lee Marshall",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Jamar Clark",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Richard Perkins",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Nathaniel Harris Pickett",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Benni Lee Tignor",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Miguel Espinal",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Michael Noel",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Kevin Matthews",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Bettie Jones",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Quintonio Leggier",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Keith Childress JR.",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Janet Wilson",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Randy Nelson",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Anthronie Scott",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Wendell Celestine",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "David Joseph",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Calin Roguemore",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Dyzhawn Perkins",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Christopher Davis",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Marco Loud",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Peter Gaines",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Torrey Robinson",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Darius Robinson",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Kevin Hicks",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Mary Truxillo ",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Demarcus Semer",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Willie Tillman",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Terrill Thomas",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Sylville Smith",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Alton Sterling",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Philando Castille",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Terence Crutcher",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Paul O’Neal",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Alteria Woods",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Jordan Edwards",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Aaron Bailey",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Ronell Foster",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Stephon Clark",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Antwon Rose II",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Botham Jean",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Pamela Turner",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Dominique Clayton",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Atatiana Jefferson",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Christopher Whitfield",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Christopher Mccorvey",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Eric Reason",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Michael Lorenzo Dean",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "Breonna Taylor",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  },
+  {
+    "fullName": "George Floyd",
+    "description": "",
+    "date": "March 2014",
+    "state": "",
+    "title": ""
+  }
+]
